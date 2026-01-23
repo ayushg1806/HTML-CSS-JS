2b32c51c168f4e5780472336ece0f84e487ad6a5
ayushg1806 <agupta9617@gmail.com>
2026-01-23 13:28:40 +0530

    Basics of JS

---
+++ JS/01-Basics_of_JS.js
@@ -0,0 +1,47 @@
+console.log("Hi, my name is Ayush!")
+console.log(25);
+
+// This is a single line comment
+
+/*
+This is a 
+multi-line comment
+*/
+
+// Variables - store, access, manipulate data
+// let, var, const
+
+let firstName = "Ayush Gupta";
+console.log(firstName)
+
+
+firstName = "John";
+console.log(firstName);
+
+
+// var - can be defined & declared again
+var firstVar = 25;
+console.log(firstVar);
+
+firstVar = 26;
+console.log(firstVar);
+
+var firstVar = 100;
+console.log(firstVar);
+
+
+// let - can be defined but not declared again
+let secVar = 30;
+console.log(secVar)
+secVar = 31;
+console.log(secVar);
+
+// let secVar = 300;
+// console.log(secVar);
+
+// const - constant value, cannot be changed
+const thirdVar = 35; 
+console.log(thirdVar);
+
+// thirdVar = 36;
+// console.log(thirdVar);
