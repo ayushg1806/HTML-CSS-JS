073771d0f45f5998aa5fce06556d0462ad3d02d6
ayushg1806 <agupta9617@gmail.com>
2026-02-08 13:20:17 +0530

    Add BookList and Book components to display a list of books (using props)

--- React/my-app/src/index.jsx
+++ React/my-app/src/index.jsx
@@ -25,4 +25,42 @@ function Footer() {
     return <footer>&copy; 2026 MySite</footer>
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Greeting />)
+const books = [
+    {   
+        id: 1,
+        author: "James Clear",
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcS_fnqMbgu47CvuouQqvMQ0NMJTi3YwkdOHnY-vx9HYAZ0Az5nE_XmpYwyb-OOmjs25WIBIuak8Pd4GOuOSq9dbkonWGSar4Q",
+        title: "Atomic Habits" 
+    },
+    {   
+        id: 2,
+        author: "Peter Thiel",
+        img: "https://m.media-amazon.com/images/I/61PDzIhVLnL._AC_UF1000,1000_QL80_.jpg",
+        title: "Zero to One"
+    }
+];
+
+const BookList = () => {
+    return (
+        <section className="bookList">
+            {books.map((book, index) => {
+                const {img, title, author} = book;
+                return(
+                    <Book {...book} key={book.id}/>
+                )
+            })}
+        </section>
+    )
+}
+
+const Book = ({author, title, img}) => {
+    return(
+        <article className="book">
+            <img src={img} alt="book-img" className="book-img" />
+            <h1>{title}</h1>
+            <h4>{author}</h4>
+        </article>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<BookList />)
