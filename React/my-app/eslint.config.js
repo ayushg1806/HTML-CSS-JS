c901cd24ff6f7c253c844123e73ff7474caf2066
ayushg1806 <agupta9617@gmail.com>
2026-02-08 13:19:30 +0530

    Disable 'no-unused-vars' rule in ESLint configuration

--- React/my-app/eslint.config.js
+++ React/my-app/eslint.config.js
@@ -23,8 +23,9 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // 'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       "react-refresh/only-export-components": "off",
+      "no-unused-vars": "off",
     },
   },
 ])
